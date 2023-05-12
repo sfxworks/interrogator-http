607707291f4d6369dd2412086e0d302c15eae0ae
Samuel Walker <sfxworks@gmail.com>
2023-05-12 06:51:34 +0000

    custom config for interrogator

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from PIL import Image
 from io import BytesIO
 from clip_interrogator import Config, Interrogator
 import base64
+import torch
 
 app = Flask(__name__)
 
 # Instantiate the Interrogator
-config = Config(clip_model_name="ViT-L-14/openai")
-# config.apply_low_vram_defaults()
+config = Config(clip_model_name="ViT-H-14/laion2b_s32b_b79k")
+config.caption_model_name = 'blip-base'
+config.caption_offload = True
+config.clip_offload = True
+config.chunk_size = 1024
+config.flavor_intermediate_count = 1024
+config.dtype = torch.float32  # Always use float32, regardless of device
 ci = Interrogator(config)
-
 
 @app.route('/interrogate', methods=['POST'])
 def interrogate():
